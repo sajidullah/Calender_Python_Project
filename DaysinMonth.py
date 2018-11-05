be6a6681848d889bc29fc212783accff22cb69f7
Sajid Ullah <sajidullah124@gmail.com>
2018-11-05 18:54:34 +0500

    Add files via upload

---
+++ DaysinMonth.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  5 18:52:08 2018
+
+@author: sajid Ullah
+"""
+
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+def days_in_month(year, month):
+    """Return number of days in that month in that year."""
+
+    # year 2017
+    # month 2
+    if not 1 <= month <= 12:
+        return 'Invalid Month'
+
+    if month == 2 and is_leap(year):
+        return 29
+
+    return month_days[month]
+
+print(days_in_month(2018, 12))
